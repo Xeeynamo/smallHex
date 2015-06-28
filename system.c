e0126f06cf4987fcb4c69cbe7812e6d38fd0d209
Luciano Ciccariello <xeeynamo@hotmail.com>
2015-06-28 16:25:01 +0200

    Low level I/O

--- system.c
+++ system.c
@@ -1,12 +1,111 @@
 #include <stdlib.h>
 #include "system.h"
 
+#if defined(_WIN32)
+#include <windows.h>
+#elif defined(PLATFORM_PSP2)
+#include <psp2/types.h>
+#include <psp2/io/fcntl.h>
+#endif
+
 void *MemoryAlloc(unsigned int size)
 {
+#if defined(_WIN32)
+	return HeapAlloc(GetProcessHeap(), 0, size);
+#else
 	return malloc(size);
+#endif
+}
+void *MemoryResize(void *mem, unsigned int newsize)
+{
+#if defined(_WIN32)
+	return HeapReAlloc(GetProcessHeap(), 0, mem, newsize);
+#else
+	return realloc(mem, size);
+#endif
 }
 void *MemoryFree(void *mem)
 {
+#if defined(_WIN32)
+	HeapFree(GetProcessHeap(), 0, mem);
+#else
 	free(mem);
+#endif
 	return NULL;
+}
+
+File FileOpen(const char *strFilename, unsigned int flags)
+{
+#if defined(_WIN32)
+	DWORD access;
+	DWORD creation = 0;
+	switch (flags & FILE_ACCESS_RW)
+	{
+	case 0:
+		access = 0;
+		break;
+	case FILE_ACCESS_READ:
+		access = GENERIC_READ;
+		break;
+	case FILE_ACCESS_WRITE:
+		access = GENERIC_WRITE;
+		break;
+	case FILE_ACCESS_RW:
+		access = GENERIC_READ | GENERIC_WRITE;
+		break;
+	}
+	if (flags & FILE_OPEN_CREATE)
+		creation |= CREATE_ALWAYS;
+	else if (flags & FILE_OPEN_TRUNCATE)
+		creation |= TRUNCATE_EXISTING;
+	HANDLE hFile = CreateFile(strFilename, access, FILE_SHARE_READ | FILE_SHARE_DELETE,
+		NULL, creation, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+		return NULL;
+	if (flags & FILE_OPEN_APPEND)
+		FileSeek((File)hFile, 0, Seek_End);
+	return (File)hFile;
+#elif defined(PLATFORM_PSP2)
+	return (File)sceIoOpen(strFilename, flags, 0777);
+#endif
+	return 0;
+}
+void FileClose(File file)
+{
+#if defined(_WIN32)
+	CloseHandle((HANDLE)file);
+#elif defined(PLATFORM_PSP2)
+	sceIoClose((SceUID)file);
+#endif
+}
+unsigned int FileRead(File file, void *data, unsigned int size)
+{
+#if defined(_WIN32)
+	DWORD read = 0;
+	ReadFile((HANDLE)file, data, size, &read, NULL);
+	return read;
+#elif defined(PLATFORM_PSP2)
+	return (unsigned int)sceIoRead((SceUID)file, data, (SceSize)size);
+#endif
+	return 0;
+}
+unsigned int FileWrite(File file, void *data, unsigned int size)
+{
+#if defined(_WIN32)
+	DWORD writed = 0;
+	WriteFile((HANDLE)file, data, size, &writed, NULL);
+	return writed;
+#elif defined(PLATFORM_PSP2)
+	return (unsigned int)sceIoWrite((SceUID)file, data, (SceSize)size);
+#endif
+	return 0;
+}
+void FileSeek(File file, signed long long offset, unsigned int mode)
+{
+#if defined(_WIN32)
+	LARGE_INTEGER large = { offset };
+	SetFilePointerEx((HANDLE)file, large, NULL, mode);
+#elif defined(PLATFORM_PSP2)
+	sceIoLseek((SceUID)file, offset, mode);
+#endif
 }
--- system.h
+++ system.h
@@ -19,7 +19,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef _SYSTEM_H
 #define _SYSTEM_H
 
+#define FILE_ACCESS_READ 0x0001
+#define FILE_ACCESS_WRITE 0x0002
+#define FILE_ACCESS_RW FILE_ACCESS_READ | FILE_ACCESS_WRITE
+#define FILE_OPEN_APPEND 0x0100
+#define FILE_OPEN_CREATE 0x0200
+#define FILE_OPEN_TRUNCATE 0x0400
+
+typedef void *File;
+
+typedef enum
+{
+	Seek_Begin,
+	Seek_Current,
+	Seek_End,
+} SeekMode;
+
 void *MemoryAlloc(unsigned int size);
 void *MemoryFree(void *mem);
+
+File FileOpen(const char *strFilename, unsigned int flags);
+void FileClose(File file);
+unsigned int FileRead(File file, void *data, unsigned int size);
+unsigned int FileWrite(File file, void *data, unsigned int size);
+void FileSeek(File file, signed long long offset, unsigned int mode);
 
 #endif
